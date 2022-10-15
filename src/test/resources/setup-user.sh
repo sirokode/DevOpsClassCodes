5a49cd1103dd9bc40bc1cea1622102ab6273ae04
sirokode <112339757+sirokode@users.noreply.github.com>
2022-10-15 16:02:35 -0500

    Update setup-user.sh

--- src/test/resources/setup-user.sh
+++ src/test/resources/setup-user.sh
@@ -105,9 +105,9 @@ update_conf()
 
 ############### MAIN ###################
 
-USER="simplex"
-GROUP="simplex"
-passw="Password1"
+USER="apollo"
+GROUP="groupx"
+passw="passwordx"
 
 if [ -f /etc/os-release ];then
    osname=`grep ID /etc/os-release | egrep -v 'VERSION|LIKE|VARIANT' | cut -d'=' -f2 | sed -e 's/"//' -e 's/"//'`
